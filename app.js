f819dffe0b3bf919d32276809a4a11f0eda4dfd1
Daniel Osborn <djorborn@gmail.com>
2018-02-12 19:40:34 -0500

    eslint app.js

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
-var express = require("express");
-var app = express();
-var mmongoose = require("./app/modules/mongoose");
+var express = require('express')
+var app = express()
 
-app.set("view engine", "pug");
-app.set("views", __dirname + "/app/views");
+app.set('view engine', 'pug')
+app.set('views', '/app/views')
 
 app.use(
-	[express.static(__dirname + "/app/public")],
-	[express.urlencoded({extended:false})],
-	[require("cookie-parser")()]
-	);
+  [express.static(require('path').join(__dirname, '/app/public'))],
+  [express.urlencoded({extended: false})],
+  [require('cookie-parser')()]
+)
 
-app.use(require("./app/routes/index"));
+app.use(require('./app/routes/index'))
 
-app.listen(3000);
+app.listen(3000)
